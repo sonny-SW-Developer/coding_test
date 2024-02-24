a4156fdb4f15c6404e1e3fad0338a73973638e2d
hyunseokSon <fairy0724@soongsil.ac.kr>
2024-02-24 18:01:42 +0900

    2차원 배열 직선 교점 구하기

---
+++ programmers_coding_test/src/programmers_coding_test/chap1/Chap1.java
@@ -0,0 +1,150 @@
+package programmers_coding_test.chap1;
+
+import java.util.*;
+
+public class Chap1 {
+    public static void main(String[] args) {
+//        System.out.println(new PhoneNumber("010-1234-5678"));
+//        System.out.println(new PhoneNumber("010 1234 5678"));
+//        System.out.println(new PhoneNumber("01012345678"));
+        Person person = new Person("손현석");
+        person.addPhoneNumber(new PhoneNumber("010-1234-5678"));
+        person.addPhoneNumber(new PhoneNumber("010 1234 5678"));
+        person.addPhoneNumber(new PhoneNumber("01012345678"));
+        System.out.println(person);
+        System.out.println("-----");
+
+        // 아래 결과값은 false가 출력된다. why?
+        // -> contains() 메서드는 equals() 메서드를 사용하여 객체를 비교한다.
+        // -> equals()는 오버라이딩이 없으면 객체가 같을 때만 true를 반환한다.
+        // -> 이때, 현재 코드에서는 서로 다른 두 객체를 이용하여 진행했으므로 false가 반환된다.
+        System.out.println(person.hasPhoneNumber(new PhoneNumber("01012345678")));
+        System.out.println("-----");
+
+        // 4번 예제
+        Person p1 = new Person("길동");
+        p1.addPhoneNumber(new PhoneNumber("010-1234-5678"));
+        p1.addPhoneNumber(new PhoneNumber("010-2345-6789"));
+
+        Person p2 = new Person("철수");
+        p2.addPhoneNumber(new PhoneNumber("010-2468-0246"));
+
+        Person p3 = new Person("영희");
+        p3.addPhoneNumber(new PhoneNumber("010-1357-9135"));
+
+        PhoneBook pb = new PhoneBook();
+        pb.addPerson(p1);
+        pb.addPerson(p2);
+        pb.addPerson(p3);
+
+        System.out.println(pb);
+        System.out.println("-----");
+
+        pb.addPerson(p3);
+        System.out.println(pb);
+    }
+
+    // 1. 전화번호 나타내기
+    private static class PhoneNumber {
+        public final String phoneNumber;
+        public PhoneNumber(String rawPhoneNum) {
+            this.phoneNumber = rawPhoneNum.replaceAll("[^0-9]", "");
+        }
+
+        public String toString() {
+            return "PhoneNumber{" + "phoneNumber = " + phoneNumber + '\'' + '}';
+        }
+
+        public boolean equals(Object o) {
+            // PhoneNumber 객체가 아니라면 false 리턴
+            if (!(o instanceof PhoneNumber)) return false;
+            return phoneNumber.equals(((PhoneNumber) o).phoneNumber);
+        }
+    }
+
+    // 2. 전화번호부 사람 나타내기
+    private static class Person {
+        public final String name;
+        private final List<PhoneNumber> numbers;
+//        private final List<String> numbers;
+
+        public Person(String name) {
+            this.name = name;
+            numbers = new ArrayList<>();
+        }
+
+        // 전화번호 추가 메서드
+        public void addPhoneNumber(PhoneNumber number) {
+            numbers.add(number);
+        }
+
+        // 전화번호 형식 검사 (위의 변수에서 List<PhoneNumber>가 아닌 List<String>인 경우
+//        public boolean addPhoneNumber(String number) {
+//            for (char c : number.toCharArray()) {
+//                if (!Character.isDigit(c)) return false;
+//            }
+//            numbers.add(number);
+//            return true;
+//        }
+
+        // 3. 사람과 전화번호 비교하기
+        public boolean hasPhoneNumber(PhoneNumber number) {
+            return numbers.contains(number);
+        }
+
+        public String toString() {
+            return "Person{" + "name=" + name + '\'' + ", numbers: " + numbers + "}";
+        }
+    }
+
+    // 4. 전화번호부 나타내기
+//    private static class PhoneBook {
+//        private final List<Person> people;
+//        private PhoneBook() {
+//            this.people = new ArrayList<>();
+//        }
+//
+//        public void addPerson(Person person) {
+//            people.add(person);
+//        }
+//
+//        public String toString() {
+//            return "PhoneBook{" + "people= " + people + "}";
+//        }
+//    }
+
+    // 4(2). 전화번호부 나타내기 - 중복 방지
+    // 4. 전화번호부 나타내기
+    private static class PhoneBook {
+        private final Set<Person> people;
+        private PhoneBook() {
+            this.people = new HashSet<>();
+        }
+
+        public void addPerson(Person person) {
+            people.add(person);
+        }
+
+        public String toString() {
+            return "PhoneBook{" + "people= " + people + "}";
+        }
+
+        // 5. 전화번호를 가진 Person 객체 반환
+        public Person search(PhoneNumber num) {
+            // stream()을 사용하여 Set<Person>을 Stream<Person>으로 변환.
+            // filter()를 사용하여 number를 가지는 Person 객체들을 찾는다.
+            // 이는 다음과 같은 for 문과 동일한 역할을 수행한다.
+//            for (Person p : people) {
+//                if (p.hasPhoneNumber(number)) {
+//                    return p;
+//                }
+//            }
+//            return null;
+            return people.stream()
+                    .filter(p -> p.hasPhoneNumber(num))
+                    .findFirst()
+                    .orElse(null);
+        }
+    }
+}
+
---
+++ programmers_coding_test/src/programmers_coding_test/chap3/chap3_1.java
@@ -0,0 +1,108 @@
+package programmers_coding_test.chap3;
+
+import java.util.*;
+
+// 교점에 점 만들기
+public class chap3_1 {
+    // 좌표 클래스
+    private static class Point {
+        public final long x,y;
+        private Point(long x, long y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // 교점 구하기
+    // a1x + b1y + c1 = 0
+    // a2x + b2y + c2 = 0 일 때 교점은
+    // x = (b1c2 - b2c1) / (a1b2 - a2b1)
+    // y = (a2c1 - a1c2) / (a1b2 - a2b1)
+    private Point intersection(long a1, long b1, long c1, long a2, long b2, long c2) {
+        double x = (double) (b1 * c2 - b2 * c1) / (a1 * b2 - a2 * b1);
+        double y = (double) (a2 * c1 - a1 * c2) / (a1 * b2 - a2 * b1);
+
+        if(x % 1 != 0 || y % 1 != 0) return null;
+        return new Point((long) x, (long) y);
+    }
+
+    // 최소 좌표 구하기
+    private Point getMinPoint(List<Point> points) {
+        long x = Long.MAX_VALUE;
+        long y = Long.MAX_VALUE;
+
+        for (Point p : points) {
+            if (p.x < x) x = p.x;
+            if (p.y < y) y = p.y;
+        }
+        return new Point(x,y);
+    }
+
+    // 최대 좌표 구하기
+    private Point getMaxPoint(List<Point> points) {
+        long x = Long.MIN_VALUE;
+        long y = Long.MIN_VALUE;
+
+        for(Point p : points) {
+            if(p.x > x) x = p.x;
+            if(p.y > y) y = p.y;
+        }
+        return new Point(x,y);
+    }
+
+    public String[] solution(int[][] line) {
+        List<Point> points = new ArrayList<>();
+        for (int i=0; i< line.length; i++) {
+            for (int j=i+1; j<line.length; j++) {
+                Point interSection = intersection(line[i][0], line[i][1], line[i][2],
+                        line[j][0], line[j][1], line[j][2]);
+                if(interSection != null) {
+                    points.add(interSection);
+                }
+            }
+        }
+
+        Point maxPoint = getMaxPoint(points);
+        Point minPoint = getMinPoint(points);
+
+        int width = (int) (maxPoint.x - minPoint.x + 1);
+        int height = (int) (maxPoint.y - minPoint.y + 1);
+
+        char[][] arr = new char[height][width];
+        for (char[] row : arr) {
+            Arrays.fill(row, ',');
+        }
+
+        for (Point p : points) {
+            int x = (int) (p.x - minPoint.x);
+            int y = (int) (p.y - minPoint.y);
+            arr[y][x] = '*';
+        }
+
+        String[] result = new String[arr.length];
+        for (int i=0; i< result.length; i++) {
+            result[i] = new String(arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("2차원 배열의 행 수를 입력하세요 : ");
+        int line = sc.nextInt();
+        int[][] arr = new int[line][3];
+        System.out.println("배열을 입력해주세요");
+        for (int i = 0; i < line; i++) {
+            System.out.print( i + "번째 행 입력 : ");
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(sc.next());
+            }
+        }
+
+        chap3_1 c3_1 = new chap3_1();
+
+        System.out.println(Arrays.toString(c3_1.solution(arr)));
+
+    }
+}
